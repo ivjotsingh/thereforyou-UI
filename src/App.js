542f28579d1334d57787ddcdbb45a2a516b5f797
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-09-12 17:54:17 +0530

    created chat and join componenets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'; 
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Join from './components/Join';
+import Chat from './components/Chat';
 
 class App extends Component{
   
     render(){
       return (
           <Router>
-              <Route path="/" exact component={Profile} />
+              <Route path="/" exact component={Join} />
               <Route path="/chat" component={Chat} />
           </Router>
       );
---
+++ src/components/Join.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'; 
+
+class Join extends Component{
+    render() {
+
+        return (
+        <div className="join">
+        <h1>hey</h1>
+        </div>
+        );
+    }
+}
+
+export default Join;
