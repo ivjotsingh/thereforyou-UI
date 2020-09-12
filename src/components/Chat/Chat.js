b61074a1d45bff597f81dc34fb7cb704c98121d0
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-09-13 02:24:37 +0530

    socket functionality added in chat for connection and real time chat

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,14 +1,34 @@
-import React, {Component} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
+import queryString from 'query-string';
+import io, { Socket } from 'socket.io-client';
 
 import './Chat.css'
 
-class Chat extends Component{
-    render() {
-        return (
-            
-            <h1> Chat </h1>
-        );
-    }
+
+const Chat = ({ location }) => {
+
+    const [name, setName] = useState('');
+    const [companion, setCompanion] = useState('');
+
+    // move to env file
+    const ENDPOINT = 'localhost:8000'
+
+    useEffect(() => {
+
+        // use props or redux insted
+        const {name, companion} = queryString.parse(location.search);
+
+        let socket = io(ENDPOINT)
+
+        setName(name);
+        setCompanion(companion);
+
+    }, [ENDPOINT, location.search])
+
+    return (
+        <h1>hi</h1>
+    );
+    
 }
 
 export default Chat;
